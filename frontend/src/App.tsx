f3b221d0e6eed7cce58e54a4403859f40ae1bb4f
sHMENGGo <jaysonpermejo123@gmail.com>
2026-02-15 00:35:46 +0800

    simple UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+	return (
+		<main className='w-svw h-svh relative ' >
+			{/* Header */}
+			<section className='w-full h-1/5  flex justify-around items-center bg-linear-to-b from-gray-500 to-black' >
+				<img src="./src/assets/otcd.png" alt="" className='h-[90%]' />
+				<input type="text" className='border border-gray-500 h-1/2 w-2/3 rounded-full p-1 px-5 text-2xl bg-black' placeholder='Search...' />
+				<FontAwesomeIcon icon={faBars} className='text-4xl' />	
+			</section>
+		</main>
+	)
 }
 
 export default App
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: true, // Allow access from network
     port: 3000, // Frontend runs on port 3000

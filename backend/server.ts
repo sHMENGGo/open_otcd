118ece99422cae54c49568906deea9d17e0e678f
Jayson Permejo <jaysonpermejo@gmail.com>
2026-03-23 16:44:23 +0800

    DB connected and prisma setup

--- backend/server.ts
+++ backend/server.ts
@@ -2,22 +2,47 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Pool } from 'pg'; // PostgreSQL client
 
 // Load environment variables (for your remote DB URL)
-dotenv.config();
+dotenv.config()
 
-const app = express();
+async function checkConnection() {
+  try {
+    const client = await pool.connect()
+    console.log("✅ Database connection successful")
+    
+    // Optional: Run a simple query to be sure
+    const res = await client.query('SELECT NOW()')
+    console.log("Server time:", res.rows[0].now)
+
+    client.release()
+  } catch (err) {
+    console.error("❌ Database connection failed:", err)
+  } finally {
+    await pool.end()
+  }
+} checkConnection()
+
+
+const app = express()
 
 // Middleware
-app.use(cors()); // Allows React to talk to this API
-app.use(express.json()); // Parses incoming JSON requests
+app.use(cors()) // Allows React to talk to this API
+app.use(express.json()) // Parses incoming JSON requests
 
 app.get('/', (req, res) => {
-  res.send('Hello! The backend is officially running.');
-});
+  res.send('Hello! The backend is officially running.')
+})
 
 // Start Server
 app.listen(3001, () => {
-  console.log(`🚀 Server ready at http://localhost:3001`);
-});
+  console.log(`🚀 Server ready at http://localhost:3001`)
+})
+
+// Create a PostgreSQL connection pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 		<main className='w-svw h-svh relative ' >
 			{/* Header */}
 			<section className='w-full h-1/5  flex justify-around items-center bg-linear-to-b from-gray-500 to-black' >
-				<img src="./src/assets/otcd.png" alt="" className='h-[90%]' />
+				<img src="./src/assets/react.svg" alt="" className='h-[60%]' />
 				<input type="text" className='border border-gray-500 h-1/2 w-2/3 rounded-full p-1 px-5 text-2xl bg-black' placeholder='Search...' />
 				<FontAwesomeIcon icon={faBars} className='text-4xl' />	
 			</section>
